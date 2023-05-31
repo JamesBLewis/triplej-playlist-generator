886bb172c7b3dc1d75f89af20f7f209263d45152
James Lewis <james.lewis2@anz.com>
2023-05-31 22:21:00 +1000

    complete refactor

---
+++ jobs/internal/run.go
@@ -0,0 +1,125 @@
+package internal
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/JamesBLewis/triplej-playlist-generator/tree/main/jobs/cmd/config"
+	"github.com/JamesBLewis/triplej-playlist-generator/tree/main/jobs/pkg/spotify"
+	"github.com/JamesBLewis/triplej-playlist-generator/tree/main/jobs/pkg/triplej"
+)
+
+type Bot struct {
+	spotifyClient     spotify.Clienter
+	triplejClient     triplej.Clienter
+	playlistSize      int
+	spotifyPlaylistId string
+}
+
+func NewBot(config config.Config) *Bot {
+	spotifyClient := spotify.NewSpotifyClient(config.SpotifyClientId, config.SpotifyClientSecret, config.SpotifyRefreshToken)
+	return &Bot{
+		spotifyClient:     spotifyClient,
+		triplejClient:     triplej.NewTiplejClient(),
+		playlistSize:      config.PlaylistSize,
+		spotifyPlaylistId: config.SpotifyPlaylistId,
+	}
+}
+
+func (b *Bot) Run(ctx context.Context) error {
+	var mappedSongs []string
+
+	recentTriplejSongs, err := b.triplejClient.FetchSongsFromTriplejAPI(b.playlistSize)
+	if err != nil {
+		log.Printf("Error fetching songs from TripleJ: %v", err)
+		return err
+	}
+	if len(recentTriplejSongs) == 0 {
+		err = errors.New("recentTriplejSongs contained 0 songs")
+		return err
+	}
+
+	currentPlaylistSongs, err := b.spotifyClient.GetCurrentPlaylist(ctx, b.spotifyPlaylistId)
+	if err != nil {
+		log.Printf("Error fetching current spotify playlist: %v", err)
+		return err
+	}
+
+	lastPlayedSong, err := b.getTrackBySongNameAndArtist(ctx, recentTriplejSongs[0])
+	if err != nil {
+		log.Printf("Could not find last triplej song on spotify: %v", err)
+		return err
+	}
+
+	// exit if the last played song on the radio was also the most recently added song to this playlist
+	if len(currentPlaylistSongs) > 0 && lastPlayedSong.Uri == currentPlaylistSongs[len(currentPlaylistSongs)-1].Uri {
+		log.Println("Exiting... playlist is already up to date with triplej")
+		return nil
+	}
+
+	log.Println("🤖diff found between playlist and triplej. updating playlist...")
+
+	mappedSongs = append(mappedSongs, lastPlayedSong.Uri)
+	err = b.updateSpotifyPlaylist(ctx, recentTriplejSongs, currentPlaylistSongs, mappedSongs)
+	if err != nil {
+		log.Printf("Error updating spotify playlist: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (b *Bot) getTrackBySongNameAndArtist(ctx context.Context, song triplej.RadioSong) (spotify.Track, error) {
+	track, err := b.spotifyClient.GetTrackBySongNameAndArtist(ctx, song.Name, song.Artists)
+	if err != nil {
+
+		log.Printf("Error getting track: %v", err)
+		return spotify.Track{}, err
+	}
+	return track, nil
+}
+
+func (b *Bot) updateSpotifyPlaylist(ctx context.Context, triplejSongs []triplej.RadioSong, SpotifySongs []spotify.Track, mappedSongs []string) error {
+	var songsToDelete []spotify.Track
+
+	for _, song := range triplejSongs[1:] {
+		tempSong, err := b.getTrackBySongNameAndArtist(ctx, song)
+		if err != nil {
+			continue
+		}
+
+		if len(SpotifySongs) > 0 && SpotifySongs[len(SpotifySongs)-1].Uri == tempSong.Uri {
+			break
+		}
+
+		if len(tempSong.Uri) > 0 {
+			// prepend item to slice
+			mappedSongs = append([]string{tempSong.Uri}, mappedSongs...)
+		}
+	}
+
+	err := b.spotifyClient.AddSongsToPlaylist(ctx, mappedSongs, b.spotifyPlaylistId)
+	if err != nil {
+		log.Printf("Error adding songs to playlist: %v", err)
+		return err
+	}
+
+	if len(mappedSongs)+len(SpotifySongs) > b.playlistSize {
+		for i := 0; i < len(mappedSongs)+len(SpotifySongs)-b.playlistSize; i++ {
+			if len(SpotifySongs[i].Uri) > 0 {
+				songsToDelete = append(songsToDelete, spotify.Track{Uri: SpotifySongs[i].Uri})
+			}
+		}
+	}
+
+	if len(songsToDelete) > 0 {
+		err = b.spotifyClient.RemoveSongsFromPlaylist(ctx, songsToDelete, b.spotifyPlaylistId)
+		if err != nil {
+			log.Printf("Error removing songs from playlist: %v", err)
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ jobs/pkg/spotify/spotify.go
@@ -0,0 +1,273 @@
+package spotify
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	ContentType = "application/json; charset=UTF-8"
+	Market      = "AU"
+)
+
+type (
+	Clienter interface {
+		GetCurrentPlaylist(ctx context.Context, playlistId string) ([]Track, error)
+		GetTrackBySongNameAndArtist(ctx context.Context, name string, artist []string) (Track, error)
+		RemoveSongsFromPlaylist(ctx context.Context, songs []Track, playlistId string) error
+		AddSongsToPlaylist(ctx context.Context, songs []string, playlistId string) error
+	}
+
+	Client struct {
+		musicAPI     string
+		accountAPI   string
+		accessToken  string
+		clientId     string
+		clientSecret string
+		refreshToken string
+		httpClient   *http.Client
+	}
+
+	PlaylistTracks struct {
+		Items []PlaylistTrackItem `json:"items"`
+	}
+
+	PlaylistTrackItem struct {
+		Track Track `json:"track"`
+	}
+
+	Track struct {
+		Uri string `json:"uri"`
+	}
+
+	TokenRefreshResponse struct {
+		AccessToken string `json:"access_token"`
+		TokenType   string `json:"token_type"`
+		Scope       string `json:"scope"`
+		ExpiresIn   int    `json:"expires_in"`
+	}
+
+	SearchTracksResponse struct {
+		Tracks SearchTracks `json:"tracks"`
+	}
+
+	SearchTracks struct {
+		Items []SearchTrackItem `json:"items"`
+	}
+
+	SearchTrackItem struct {
+		Uri  string `json:"uri"`
+		Name string `json:"name"`
+	}
+)
+
+//go:generate mockgen -destination=mocks/spotify.go -source=spotify.go
+
+func NewSpotifyClient(clientId, clientSecret, refreshToken string) Clienter {
+	return &Client{
+		musicAPI:     "https://api.spotify.com/v1",
+		accountAPI:   "https://accounts.spotify.com/api",
+		clientId:     clientId,
+		clientSecret: clientSecret,
+		refreshToken: refreshToken,
+		accessToken:  "",
+		// yes this is an arbitrary timeout I've pulled out of thin air
+		httpClient: &http.Client{Timeout: 10 * time.Second},
+	}
+}
+
+// Do wraps httpClient.Do and injects an access token into the request's header
+func (sc *Client) Do(req *http.Request) (*http.Response, error) {
+	// if no access token is set, refresh it.
+	if sc.accessToken == "" {
+		if err := sc.refreshAccessToken(); err != nil {
+			return nil, err
+		}
+	}
+
+	// Set the Authorization header to use the new access token
+	req.Header.Set("Authorization", "Bearer "+sc.accessToken)
+
+	// Now send the request using the http.Client
+	return sc.httpClient.Do(req)
+}
+
+func (sc *Client) refreshAccessToken() error {
+	fmt.Println("Refreshing Spotify access token...")
+
+	encodedIdAndSecret := base64.StdEncoding.EncodeToString([]byte(sc.clientId + ":" + sc.clientSecret))
+	data := url.Values{}
+	data.Set("grant_type", "refresh_token")
+	data.Set("refresh_token", sc.refreshToken)
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, sc.accountAPI+"/token", strings.NewReader(data.Encode()))
+	if err != nil {
+		return fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", "Basic "+encodedIdAndSecret)
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	res, err := sc.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	tokenRefreshResponse := &TokenRefreshResponse{}
+	if err := json.NewDecoder(res.Body).Decode(tokenRefreshResponse); err != nil {
+		return fmt.Errorf("failed to unmarshal response body: %w", err)
+	}
+
+	sc.accessToken = tokenRefreshResponse.AccessToken
+	return nil
+}
+
+func (sc *Client) GetCurrentPlaylist(ctx context.Context, playlistId string) ([]Track, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/playlists/%s/tracks", sc.musicAPI, playlistId), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	res, err := sc.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	playlistTracks := &PlaylistTracks{}
+	if err := json.NewDecoder(res.Body).Decode(playlistTracks); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response body: %w", err)
+	}
+
+	var songs []Track
+	for _, item := range playlistTracks.Items {
+		songs = append(songs, Track{Uri: item.Track.Uri})
+	}
+	return songs, nil
+}
+
+func (sc *Client) GetTrackBySongNameAndArtist(ctx context.Context, name string, artists []string) (Track, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, sc.musicAPI+"/search", nil)
+	if err != nil {
+		return Track{}, fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	query := req.URL.Query()
+	query.Add("q", fmt.Sprintf("%s %s", name, strings.Join(artists, ", ")))
+	query.Add("type", "track")
+	query.Add("market", Market)
+	query.Add("limit", "1")
+	req.URL.RawQuery = query.Encode()
+
+	res, err := sc.Do(req)
+	if err != nil {
+		return Track{}, fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return Track{}, fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	searchTracksResponse := &SearchTracksResponse{}
+	if err := json.NewDecoder(res.Body).Decode(searchTracksResponse); err != nil {
+		return Track{}, fmt.Errorf("failed to unmarshal response body: %w", err)
+	}
+
+	if len(searchTracksResponse.Tracks.Items) == 0 {
+		return Track{}, fmt.Errorf("could not find track: %s %s", name, strings.Join(artists, ", "))
+	}
+	return Track{
+		Uri: searchTracksResponse.Tracks.Items[0].Uri,
+	}, nil
+}
+
+func (sc *Client) RemoveSongsFromPlaylist(ctx context.Context, songs []Track, playlistId string) error {
+	if len(songs) == 0 {
+		return nil
+	}
+
+	// Create a struct to hold the tracks data
+	type playlistData struct {
+		Tracks []Track `json:"tracks"`
+	}
+
+	// Fill the struct with our songs data
+	data := playlistData{Tracks: songs}
+
+	// Marshal the struct into JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("failed to marshal songs: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, fmt.Sprintf("%s/playlists/%s/tracks", sc.musicAPI, playlistId), bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", ContentType)
+
+	res, err := sc.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to execute request: %w", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	return nil
+}
+
+func (sc *Client) AddSongsToPlaylist(ctx context.Context, songs []string, playlistId string) error {
+	if len(songs) == 0 {
+		return nil
+	}
+
+	jsonList, err := json.Marshal(songs)
+	if err != nil {
+		return fmt.Errorf("failed to marshal songs: %w", err)
+	}
+	jsonData := fmt.Sprintf(`{"uris":%s}`, string(jsonList))
+	if err != nil {
+		return fmt.Errorf("failed to marshal songs: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/playlists/%s/tracks", sc.musicAPI, playlistId), bytes.NewBuffer([]byte(jsonData)))
+	if err != nil {
+		return fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", ContentType)
+
+	res, err := sc.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		return fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	return nil
+}
